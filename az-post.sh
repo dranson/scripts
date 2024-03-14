7d5f9a99697648c1aded293ee59780acbd6bfa7f
dranson <david.ranson@yahoo.co.uk>
2024-03-14 09:48:31 +0000

    Update az-post.sh - WEKA login

--- az-post.sh
+++ az-post.sh
@@ -2,11 +2,15 @@
 sudo apt get update 
 sudo apt install -y pdsh git
 
+#Login to WEKA Admin
+read -sp 'WEKA Admin Password: ' passvar
+weka user login admin $passvar
+
 #Setup PDSH variables
 echo 'export PDSH_RCMD_TYPE=ssh' >> ~/.bashrc
 echo 'export WCOLL=~/weka_cluster.host'  >> ~/.bashrc
-weka cluster servers list -o hostname --no-header > ~/weka_cluster.host
-source .bashrc
+weka cluster servers list --role=backend -o hostname --no-header > ~/weka_cluster.host
+weka cluster servers list --role=client -o hostname --no-header > ~/weka_clients.host
 
 #Setup SSH 
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
