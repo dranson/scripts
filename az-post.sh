7c50d912e9f59171ba4114392f9c4f61ede63f6e
dranson <david.ranson@yahoo.co.uk>
2026-01-15 10:03:54 +0000

    Update az-post.sh
    FIxes for new post client install option

--- az-post.sh
+++ az-post.sh
@@ -4,21 +4,18 @@
 sudo apt update 
 sudo apt install -y pdsh git
 
-#Login to WEKA Admin
-echo 'read here'
-read -s -p 'WEKA Admin Password: ' passvar
-weka user login admin $passvar
-echo 'read done'
-
 #Setup PDSH variables
+weka user login david AZURe_*321!
 echo 'export PDSH_RCMD_TYPE=ssh' >> ~/.bashrc
 echo 'export WCOLL=~/weka_cluster.host'  >> ~/.bashrc
 weka cluster servers list --role=backend -o hostname --no-header > ~/weka_cluster.host
 weka cluster servers list --role=client -o hostname --no-header > ~/weka_clients.host
+weka user delete david
 
 #Setup SSH 
-echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> /home/weka/.ssh/config
+chown weka /home/weka/.ssh/config;chmod 400 /home/weka/.ssh/config
 
 # Install WEKA Tools
-mkdir ~/tools
-git clone https://github.com/weka/tools ~/tools
+mkdir /home/weka/tools
+git clone https://github.com/weka/tools /home/weka/tools
