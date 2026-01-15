6837bd628c8d7c775e2696d864332acac315b0e7
dranson <david.ranson@yahoo.co.uk>
2026-01-15 14:59:03 +0000

    Update az-client-post.sh
    add fio to apt install

--- az-client-post.sh
+++ az-client-post.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 #Install Packages
-sudo apt update 
-sudo apt install -y pdsh git
+apt update 
+apt install -y pdsh fio git
 
 #Setup PDSH variables
 weka user login david AZURe_*321!
@@ -13,9 +13,8 @@ weka cluster servers list --role=client -o hostname --no-header > /home/weka/wek
 weka user delete david
 
 #Setup SSH 
-echo "   StrictHostKeyChecking no" >> /home/weka/.ssh/config
-chown weka /home/weka/.ssh/config;chmod 400 /home/weka/.ssh/config
+sudo -u weka echo "   StrictHostKeyChecking no" >> /home/weka/.ssh/config
 
 # Install WEKA Tools
-mkdir /home/weka/tools
-git clone https://github.com/weka/tools /home/weka/tools
+sudo -u weka mkdir /home/weka/tools
+sudo -u weka git clone https://github.com/weka/tools /home/weka/tools
