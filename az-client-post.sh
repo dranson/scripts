07c6b1413c349111e3f1c550ccb2581d4e34b405
dranson <david.ranson@yahoo.co.uk>
2026-01-15 14:42:12 +0000

    Create az-client-post.sh

---
+++ az-client-post.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+#Install Packages
+sudo apt update 
+sudo apt install -y pdsh git
+
+#Setup PDSH variables
+weka user login david AZURe_*321!
+echo 'export PDSH_RCMD_TYPE=ssh' >> /home/weka/.bashrc
+echo 'export WCOLL=~/weka_cluster.host'  >> /home/weka/.bashrc
+weka cluster servers list --role=client -o hostname --no-header > /home/weka/weka_clients.host
+weka user delete david
+
+#Setup SSH 
+echo "   StrictHostKeyChecking no" >> /home/weka/.ssh/config
+chown weka /home/weka/.ssh/config;chmod 400 /home/weka/.ssh/config
+
+# Install WEKA Tools
+mkdir /home/weka/tools
+git clone https://github.com/weka/tools /home/weka/tools
