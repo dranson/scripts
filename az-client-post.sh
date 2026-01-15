d18251773db43f7810273319b2e6789b9bb25a11
dranson <david.ranson@yahoo.co.uk>
2026-01-15 15:33:55 +0000

    Update az-client-post.sh
    fio path fixes

--- az-client-post.sh
+++ az-client-post.sh
@@ -9,7 +9,6 @@ weka user login david AZURe_*321!
 echo 'export PDSH_RCMD_TYPE=ssh' >> /home/weka/.bashrc
 echo 'export WCOLL=~/weka_client.host'  >> /home/weka/.bashrc
 weka cluster servers list --role=client -o hostname --no-header > /home/weka/weka_clients.host
-weka user delete david
 
 #Setup SSH 
 sudo -u weka echo "   StrictHostKeyChecking no" >> /home/weka/.ssh/config
@@ -22,7 +21,7 @@ sudo -u weka git clone https://github.com/weka/tools /home/weka/tools
 chmod 777 /mnt/weka
 mkdir /mnt/weka/fio
 sudo -u weka mkdir /home/weka/fio
-sudo -u weka curl -o /home/weka/fio -s https://raw.githubusercontent.com/dranson/scripts/main/fio-read.job
-sudo -u weka curl -o /home/weka/fio -s https://raw.githubusercontent.com/dranson/scripts/main/fio-write.job
-sudo -u weka curl -o /home/weka/fio -s https://raw.githubusercontent.com/dranson/scripts/main/fio-read-iops.job
-sudo -u weka curl -o /home/weka/fio -s https://raw.githubusercontent.com/dranson/scripts/main/fio-write-iops.job
+sudo -u weka curl -o /home/weka/fio/fio-read.job https://raw.githubusercontent.com/dranson/scripts/main/fio-read.job
+sudo -u weka curl -o /home/weka/fio/fio-write.job https://raw.githubusercontent.com/dranson/scripts/main/fio-write.job
+sudo -u weka curl -o /home/weka/fio/fio-read-iops.job https://raw.githubusercontent.com/dranson/scripts/main/fio-read-iops.job
+sudo -u weka curl -o /home/weka/fio/fio-write-iops.job https://raw.githubusercontent.com/dranson/scripts/main/fio-write-iops.job
