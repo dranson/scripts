7326b869f96b2490efdfe4873a3af13f7ba494de
dranson <david.ranson@yahoo.co.uk>
2026-01-16 12:42:05 +0000

    Update az-client-post.sh
    typo fix in in WCOLL entry for .bashrc

--- az-client-post.sh
+++ az-client-post.sh
@@ -7,7 +7,7 @@ apt install -y pdsh fio git
 #Setup PDSH variables
 weka user login david AZURe_*321!
 echo 'export PDSH_RCMD_TYPE=ssh' >> /home/weka/.bashrc
-echo 'export WCOLL=~/weka_client.host'  >> /home/weka/.bashrc
+echo 'export WCOLL=~/weka_clients.host'  >> /home/weka/.bashrc
 weka cluster servers list --role=client -o hostname --no-header > /home/weka/weka_clients.host
 
 #Setup SSH 
